1ebf9c2f9efaf7ea9fdd28624b44995cb48b5770
jivanrod <jivanrod@gmail.com>
2024-12-18 21:41:30 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../src/error_handler.php';
+include_once '../src/error_handler.php';
 require_once '../src/bd/BD.php';
 require_once '../src/modelo/Banco.php';
 require_once '../src/modelo/Cliente.php';
